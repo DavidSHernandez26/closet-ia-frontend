273eecc20199562cdd62ae91118471b56b09da8e
David Hernandez <davidhernandezuwu26@gmail.com>
2026-05-07 16:20:05 -0500

    ...

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.closetia.app',
-  appName: 'Closet IA',
+  appName: 'Be: Confident',
   webDir: 'build',
   server: {
     androidScheme: 'https',
